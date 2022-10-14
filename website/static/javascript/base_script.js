d0dbc2e0a34bf74c360ca446e7b018b63d33055d
Armon <armon@nudgex.net>
2022-10-13 23:52:47 -0700

    first commit

---
+++ website/static/javascript/base_script.js
@@ -0,0 +1,18 @@
+// set active nav link
+//let nav_links = document.getElementsByClassName('nav-link')
+//let ind = 0;
+//for(i=0;i<nav_links["length"];i++){
+//	console.log(i,nav_links[i])
+//	if(nav_links[i].href == document.URL){
+//	nav_links[i].classList.add('active');
+//	nav_links[i].ariaCurrent="page"; //good for accessibility purposes, but doesn't work in firefox.
+//	}
+//}
+//
+//
+//const nav_link_array = Array.from(nav_links)
+//nav_link_array.forEach(nav_link => {
+//	nav_link.onclick = function(){
+//		nav_link.classList.add('active')
+//	}
+//})
